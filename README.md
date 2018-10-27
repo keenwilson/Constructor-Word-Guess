bfa7004bc2644e3aa2ac21de647ee71f7a5459c5
Keen Wilson <40321520+keenwilson@users.noreply.github.com>
2018-10-26 21:41:13 -0500

    Update README

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Constructor-Word-Guess
 A command-line (*CLI*) word guess game using `constructor` functions to create objects representing each word to guess as well as each letter in each word, and using the `inquirer` package to prompt the user to guess a letter.
 
+---
 ## How to Play
 In this game, you will be given a certain amount of guesses. (Perhaps: 10 or 20 depending on the level of difficulty)
 For each word, you will be asked to guess an underlying character (A to Z).
@@ -8,10 +9,10 @@ If you guess matches the underlying characters of the word -- the game prints "C
 If your guess does not match the underlying characters of the word -- the game prints "Incorrect" and reduces the amount of guesses remaining by 1.
 The game ends when you get to 0 guesses remaining or have no guesses left.
 
-
+---
 ## Modules
 * **Index**
-    * The Index module creates a new Game object and calls "playGame" method which will run the game
+    * The Index module creates a new Game object and calls `playGame` method which will run the game
 * **Letter**
     * The Letter module is a constructor function for displaying either an underscore or the underlying character for each letter in the word depending on whether or not the user has guessed the letter.
     * The Letter constructor has the following properties and methods: 
@@ -24,22 +25,24 @@ The game ends when you get to 0 guesses remaining or have no guesses left.
     * The Word module is a constructor function for creating an object representing the current word the user is attempting to guess.
     * The Word constructor has with the following properties and methods:
         * `letters`:
-        * `prototype.toString`: Method for all Word objects on the protoype which returns a string representing the word. Because we name a function `toString`, JavaScript automatically call `toString` on each letter, then joins them together even if we don't call `toString` separately. In this app, `Word.prototype.toString`. call the `letters` method on each letter object that will display either the underlying character or an underscore, then `.join(" ")` concatenates those letters/placeholders together.
-        * `prototype.hasThisLetter`: Method for all Word objects on the protoype which takes a character as an argument and calls the `Letter.prototype.didExistinCurrentWord` function on the letter object to see whether that guessed letter exists in the current word. This method will return a boolean (`true` or `false`) value.
-        * `prototype.guessedCorrectly`: Method for all Word objects on the protoype which returns `true` if all letters in the word have been guessed correctly.
-        *`prototype.getSolution`: Method for all Word objects on the protoype which calls the underlying characters and join them together in order to show the user the correct answer.
+        * `prototype.toString`: Method for all Word objects on the protoype which returns a string representing the word. Because we name a function `toString`, JavaScript automatically call `toString` on each letter, then joins them together even if we don't call `toString` separately. In this app, `Word.prototype.toString`. call the `letters` method on each letter object that will display either the underlying character or an underscore, then `.join(" ")` concatenates those letters/placeholders together
+        * `prototype.hasThisLetter`: Method for all Word objects on the protoype which takes a character as an argument and calls the `Letter.prototype.didExistinCurrentWord` function on the letter object to see whether that guessed letter exists in the current word. This method will return a boolean (`true` or `false`) value
+        * `prototype.guessedCorrectly`: Method for all Word objects on the protoype which returns `true` if all letters in the word have been guessed correctly
+        *`prototype.getSolution`: Method for all Word objects on the protoype which calls the underlying characters and join them together in order to show the user the correct answer
 * **Game**
+    * The Game constructor is used to create a game object and house the game logic.
     * The Game module is a constructor function with the following properties and methods: 
-            * `playGame`: 
-            * `newWord`: 
-            * `makeGuess`:
-    
-    * Once the user's guess is evaluated, it should print the result to the console.
-
+            * `playGame`: Method which, when the game starts, will reset number of guesses remaining to 10, empty an array of letters already guessed, and random a new word for the user to play
+            * `newWord`: Method which tells the user to guess a word in `Pizza toppings` category, randoms a word from `wordsToPick.js`, creates a new `Word` object using that random word, and runs `makeGuess`
+            * `makeGuess`: Method which shows the current status of the word in a box, prompts the user to guess a letter using `guessALetter` method, and then checks if the user has any guesses remaining using `checkRound`
+            *`guessALetter`: Method which prompts the user to guess a letter using the `inquirer` package, checks if the user guess correctly, then displays either "CORRECT" or "INCORRECT"
+            *`checkRound`: Method which checks if the user has any guesses remaining
+            *`askIfStartOver`: Method which asks the user if they would like to play again
+            *`endGame`: Method which exits from the current Node.js process by using `process.exit(0)`
 * **WordsToPick**
     * The WordsToPick module is an array of all words that can be chosen. In this app, these words are belong to **Delicously Popular Pizza Toppings**
 * **Pizza**
-    * The Pizza module holds the pizza emoji '🍕'. 
+    * The Pizza module exports the Pizza emoji '🍕' so that we can use it in `game.js` . 
     
 ---
 ## Author
--- game.js
+++ game.js
@@ -7,15 +7,10 @@
 // For each new random word, you will be asked to guess an underlying character (A to Z).
 // If you guess matches the underlying characters of the word -- the game prints "Correct" and displays that character at its position(s) in the word.
 // If your guess does not match the underlying characters of the word -- the game prints "Incorrect" and reduces the amount of guesses remaining by 1.
-
-/*
-// For each new word, the game will reset your guesses remaining to 10.
-*/
-
 // The game ends when you get to 0 guesses remaining or have no guesses left.
 // ===========================================================================================================
 // Loads the inquirer package to take in user commands
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
 // includes our Word module exported from word.js
 const Word = require("./Word");
 // Includes our wordsToPick for a lits of words
@@ -23,16 +18,18 @@ const wordsToPick = require("./wordsToPick");
 // Includes ansi-colors, pizza, figlet, and boxen for decorating the command line
 const c = require('ansi-colors');
 const Pizza = require("./pizza");
-const figlet = require("figlet");
+const figlet = require('figlet');
 const boxen = require('boxen');
 
 // The Game constructor is used to create game object and house the game logic
 function Game() {
 
-    // this.game will hold all of our game
-    this.game = this;
-    // `self` gives access to the current Word { letters: [ Letter Object, Letter Object,...]}
-    const self = this;
+    // `game` will gives access to the current Word objects { letters: [ Letter object, Letter object,...]}
+    const game = this;
+
+    // Starts with an empty array of Letters Already Guessed
+    // This property will be reset for every word played
+    this.lettersAlreadyGuessed = [];
 
     // When the game starts, reset number of guesses remaining to 10, empty an array of letters already guessed, and random a new word
     this.playGame = function () {
@@ -47,7 +44,7 @@ function Game() {
         // Greeting message tells the user to guess a word in `Pizza toppings` category
         var pizza = new Pizza();
         console.log(boxen((`${pizza.emoji} ${pizza.emoji} ${pizza.emoji} Deliciously Popular Pizza Toppings ${pizza.emoji} ${pizza.emoji} ${pizza.emoji}`), { padding: 1, margin: 1, borderStyle: 'round' }));
-        console.log(c.yellow("\nYou have " + self.guessesRemaining + " guesses remaining to play! \n"));
+        console.log(c.yellow("\nYou have " + game.guessesRemaining + " guesses remaining to play! \n"));
         // Random a word from wordsToPick.js
         var randomWord = wordsToPick[Math.floor(Math.random() * wordsToPick.length)];
         // Creates a new Word object using a random word from the array
@@ -56,18 +53,17 @@ function Game() {
         this.makeGuess();
     };
 
-    // Starts with an empty array of Letters Already Guessed
-    // This property will be reset for every word played
-    this.lettersAlreadyGuessed = [];
 
-    // Prompt the user to guess a letter
+
+    // Prompt the user to guess a letter then check if the user has any guesses remaining 
+    // If the number of guesses remaining is not less than 1 (get to 0 guesses remaining), run the next .makeGuess
     this.makeGuess = function () {
         // Show the current status of the word 
-        
         console.log(boxen(("\n" + this.currentWord + "\n"),{ padding: 1, margin: 1, borderStyle: 'round' }));
-        // Promp the user to guess a letter then check if the user has any guesses remaining 
+        // Prompts the user to guess a letter 
         this.guessALetter().then(function() {
-            self.checkRound()
+            // then check if the user has any guesses remaining 
+            game.checkRound()
         });
     };
 
@@ -88,26 +84,26 @@ function Game() {
                     pattern: /[a-zA-Z]/gi
                 }
             ])
-            .then(function (inquirerResponse) {
+            .then(function (answer) {
 
                 // Add the user's guess in the letters already guessed
-                var userGuess = inquirerResponse.userGuess.toUpperCase()
+                var userGuess = answer.userGuess.toUpperCase()
                 console.log("You have guessed " + c.bgCyan(" " + userGuess + " "));
 
-                self.lettersAlreadyGuessed.push(userGuess);
-                console.log("Letters Already Guessed " + c.cyan(" " + self.lettersAlreadyGuessed + " "));
+                game.lettersAlreadyGuessed.push(userGuess);
+                console.log("Letters Already Guessed " + c.cyan(" " + game.lettersAlreadyGuessed + " "));
 
 
                 // If the letter the user are trying to guess is in the current word, log that they are CORRECT
-                var isCorrect = self.currentWord.hasThisLetter(userGuess);
+                var isCorrect = game.currentWord.hasThisLetter(userGuess);
                 if (isCorrect) {
                     console.log(c.bold.green("\nCORRECT!!!\n"));
                 }
                 else {
                     // Otherwiss, reduce guesses remaining by 1
-                    self.guessesRemaining--;
+                    game.guessesRemaining--;
                     //  tell user they are INCORRECT (letter is not in the word) and display how many guesses left
-                    console.log(c.bold.red("\nINCORRECT!!!\n" + c.inverse.yellow(" " + self.guessesRemaining + " guesses remaining!!! \n")));
+                    console.log(c.bold.red("\nINCORRECT!!!\n" + c.inverse.yellow(" " + game.guessesRemaining + " guesses remaining!!! \n")));
                 }
             });
     };
@@ -116,30 +112,28 @@ function Game() {
     this.checkRound = function () {
 
         // If the user has no guesses remaining after this guess, , 
-        if (self.guessesRemaining < 1) {
+        if (game.guessesRemaining < 1) {
             // Show the user the correct word
             console.log(c.yellow("No guesses left! The word was: \"" + self.currentWord.getSolution() + "\"\n"));
             // Create a prompt to ask if the user want to start over
-            self.askToStartOver();
+            game.askIfStartOver();
         }
-        else if (self.currentWord.guessedCorrectly()) {
+        else if (game.currentWord.guessedCorrectly()) {
             // If the user guessed all letters of the current word correctly, reset guessesLeft to 10 and get the next word
-            console.log(c.green(" You got it right. The answer is " + self.currentWord.getSolution() + "! \n Next word! \n\n"));
-/*             // Update guess remaining for the current user to 10
-            self.guessesRemaining = 10; */
+            console.log(c.green(" You got it right. The answer is " + game.currentWord.getSolution() + "! \n Next word! \n\n"));
             // Empty Letters Already Guessed
-            self.lettersAlreadyGuessed = [];
+            game.lettersAlreadyGuessed = [];
             // Create a new Word object
-            self.newWord();
+            game.newWord();
         }
         else {
             // Otherwise prompt the user to guess the next letter
-            self.makeGuess();
+            game.makeGuess();
         }
     };
 
     // After no guesses remaining, ask the user if they would like to play again.
-    this.askToStartOver = function () {
+    this.askIfStartOver = function () {
         // Create a "Prompt" asking the user if they want to start over
         inquirer
             .prompt([
@@ -151,15 +145,15 @@ function Game() {
 
                 }
             ])
-            .then(function (inquirerResponse) {
+            .then(function (answer) {
                 // If the user says yes, start the new game, otherwise quit the game
-                if (inquirerResponse.confirm) {
+                if (answer.confirm) {
                     // Runs playGame from the start once more if they do
-                    self.playGame();
+                    game.playGame();
                 }
                 else {
                     // If the user says no, end the game 
-                    self.endGame();
+                    game.endGame();
                 }
             });
     };
